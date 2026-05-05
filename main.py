5154de33bfbdff065caca9ae9c7cd448cbd01593
kristiyan <kristiyan.iliev@dxc.com>
2026-05-05 10:33:43 +0100

    gathering the list of docs

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ response = requests.get("https://superdoc.bg", verify=False)
 
 if response:
     driver.get("https://superdoc.bg")
-    specialty = Select(driver.find_element(By.NAME, "specialty_id"))
+    specialty = Select(driver.find_element(By.ID, "specialty_id"))
     specialty.select_by_visible_text("Гастроентеролог")
+    town = Select(driver.find_element(By.ID,"location"))
+    town.select_by_visible_text("София град")
+    doc_name = Select(driver.find_element(By.ID,"doc_name"))
+    doc_name.select_by_visible_text("Спасова")
+    driver.find_element(By.CLASS_NAME, "btn btn-primary btn-lg align-self-start").click()
+    driver.implicitly_wait(5)
+    results = driver.find_elements(By.ID, "search-results-wrap")
+
 else:
     print("Website not reachable.")
