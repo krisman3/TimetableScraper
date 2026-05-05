e2e1231d75035596de6af5243a08a3c6763ee2f4
kristiyan <kristiyan.iliev@dxc.com>
2026-05-05 10:27:47 +0100

    starting filtering

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome()
 
-response = requests.get("www.superdoc.bg")
+response = requests.get("https://superdoc.bg", verify=False)
 
 if response:
-    pass
+    driver.get("https://superdoc.bg")
+    specialty = Select(driver.find_element(By.NAME, "specialty_id"))
+    specialty.select_by_visible_text("Гастроентеролог")
 else:
     print("Website not reachable.")
